08a7c60f1ea891ddc13845efe8d63a5f7a36844e
Link <link@prototipos3d.com.br>
2023-05-18 20:57:57 -0300

    lesson 3: server and web authentication with github and jwt

---
+++ server/src/routes/auth.ts
@@ -0,0 +1,82 @@
+import { FastifyInstance } from 'fastify'
+import { z } from 'zod'
+import { prisma } from '../lib/prisma'
+import axios from 'axios'
+
+export async function authRoutes(app: FastifyInstance) {
+  app.post('/register', async (request) => {
+    const bodySchema = z.object({
+      code: z.string(),
+    })
+
+    const { code } = bodySchema.parse(request.body)
+    console.log('Recieved code', code)
+
+    const accessTokenResponse = await axios.post(
+      'https://github.com/login/oauth/access_token',
+      null,
+      {
+        params: {
+          client_id: process.env.GITHUB_CLIENT_ID,
+          client_secret: process.env.GITHUB_CLIENT_SECRET,
+          code,
+        },
+        headers: {
+          Accept: 'application/json',
+        },
+      },
+    )
+
+    const { access_token } = accessTokenResponse.data
+    console.log('Access Token', access_token)
+
+    const userResponse = await axios.get('https://api.github.com/user', {
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+      },
+    })
+    // console.log(userResponse.data)
+
+    const userSchema = z.object({
+      id: z.number(),
+      login: z.string(),
+      name: z.string(),
+      avatar_url: z.string().url(),
+    })
+
+    const userInfo = userSchema.parse(userResponse.data)
+
+    let user = await prisma.user.findUnique({
+      where: {
+        githubId: userInfo.id,
+      },
+    })
+
+    if (!user) {
+      user = await prisma.user.create({
+        data: {
+          githubId: userInfo.id,
+          login: userInfo.login,
+          name: userInfo.name,
+          avatarUrl: userInfo.avatar_url,
+        },
+      })
+    }
+
+    const token = app.jwt.sign(
+      {
+        name: user.name,
+        avatarUrl: user.avatarUrl,
+      },
+      {
+        sub: user.id,
+        expiresIn: '30 days',
+      },
+    )
+    console.log('Token', token)
+
+    return {
+      token,
+    }
+  })
+}
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,22 @@
+import 'dotenv/config'
+
 import fastify from 'fastify'
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
 import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from './routes/auth'
 
 const app = fastify()
 
 app.register(cors, {
   origin: true, // todas URLS podem acessar. Setup com somente as desejadas: ['http://localhost:3000', 'http://rocketseat.com.br/']
 })
+
+app.register(jwt, {
+  secret: 'spacetime',
+})
+
+app.register(authRoutes)
 app.register(memoriesRoutes)
 
 app
