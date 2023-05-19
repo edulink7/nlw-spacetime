6b4b974a0a329695723eb484d9b858752e3b591d
Link <link@prototipos3d.com.br>
2023-05-19 20:49:43 -0300

    lesson 4: web - layout, logout, auth middleware and new memory page

--- web/src/app/api/auth/callback/route.ts
+++ web/src/app/api/auth/callback/route.ts
@@ -6,20 +6,22 @@ export async function GET(request: NextRequest) {
   const code = searchParams.get('code')
   console.log(code)
 
+  const redirectTo = request.cookies.get('redirectTo')?.value
+
   const registerResponse = await api.post('/register', {
     code,
   })
 
   const { token } = registerResponse.data
 
-  const redirectURL = new URL('/', request.url)
+  const redirectURL = redirectTo ?? new URL('/', request.url)
 
   // max-age in seconds = seconds * minutes * hours * days
   const cookieExpiresInSeconds = 60 * 60 * 24 * 30
 
   return NextResponse.redirect(redirectURL, {
     headers: {
-      'Set-Cookie': `token=${token}; Path=/; max-age=${cookieExpiresInSeconds}`,
+      'Set-Cookie': `token=${token}; Path=/; max-age=${cookieExpiresInSeconds};`,
     },
   })
 }
